a26225414d9a5a82cbdce76504f0a8e85c1b00c2
Aaqil101 <aaqil101@users.noreply.github.com>
2025-01-11 17:04:10 +0530

    Remove unused classes

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -12,6 +12,5 @@ __all__ = [
     "add_driver_var", "CompositorNodeNames", "hexcode_to_rgb",
     "hex_color_add", "Color", "OldEevee_Bloom_Names",
     "OldEevee_Bloom_Descr", "ensure_connection", "poll_view_3d",
-    "UpdateRTCompositingNames", "update_real_time_compositing", "ToggleOeBloomNames",
-    "toggle_oldeevee_bloom", "is_compositor_enabled"
+    "update_real_time_compositing", "toggle_oldeevee_bloom", "is_compositor_enabled"
 ]
